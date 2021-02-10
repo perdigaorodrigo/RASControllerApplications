418951afcd99b0bc132da4bf4f20876986eb6cb0
perdigaorodrigo <60547290+perdigaorodrigo@users.noreply.github.com>
2021-02-10 18:29:54 -0300

    Add files via upload
    Código utilizado no primeiro video da série "Utilizando Python para controlar o HEC-RAS"
    Permite: 
    -Importação da biblioteca de funções do HECRASController 
    -Abertura e fechamento automático da janela do HEC-RAS(ShowRAS ; QuitRAS)
    -Execução automática de simulação (Compute_CurrentPlan)

---
+++ VIDEO_01.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 22:12:05 2021
+
+@author: Rodrigo
+"""
+#IMPORTANDO BIBLIOTECA
+import win32com.client
+
+#ATRIBUINDO FUNCOES DO HECRASCONTROLLER AO OBJETO RC
+RC=win32com.client.Dispatch("RAS507.HECRASCONTROLLER")
+
+#ABRINDO E FECHANDO JANELA
+RC.ShowRAS()
+
+RC.QuitRAS()
+
+#ABRINDO PROJETO
+RC.Project_Open(r"C:\Users\Rodrigo\Desktop\HECRAS\DADOS\RAS\1D_MODEL.prj")
+
+#EXECUTANDO SIMULACAO DO PLANO ATUAL
+Simulacao=RC.Compute_CurrentPlan(None,None,True)
+
+#SALVANDO PROJETO
+RC.Project_Save()
+
+#FECHANDO JANELA
+RC.QuitRAS()
+
+
+
+
+
